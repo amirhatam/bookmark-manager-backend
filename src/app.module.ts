e87ffb54b3066bb34eec0cc784add5e6f0ed8145
Amir <amirhatam136@gmail.com>
2022-02-28 11:25:48 +0100

    create bookmark controller & module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { BookmarksModule } from './bookmarks/bookmarks.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [BookmarksModule],
 })
 export class AppModule {}
