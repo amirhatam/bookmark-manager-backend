3efd403f0e13d377f55724cc99699373dd23037d
Amir <amirhatam136@gmail.com>
2022-03-12 18:18:04 +0100

    create getBookmarkDto & add Query Parameter for find a bookmark

--- src/bookmarks/bookmarks.controller.ts
+++ src/bookmarks/bookmarks.controller.ts
@@ -1,16 +1,25 @@
 import { Bookmark } from './bookmark.model';
 import { BookmarksService } from './bookmarks.service';
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
 import { url } from 'inspector';
 import { CreatBookmarkDto } from './dto/create-bookmark.dto';
+import { GetBookmarkDto } from './dto/get-bookmark.dto';
 
 @Controller('bookmarks')
 export class BookmarksController {
   constructor(private bookmarksService: BookmarksService) {}
 
   @Get()
-  findAll(): Bookmark[] {
+  find(@Query() getBookmarkDto: GetBookmarkDto): Bookmark[] {
+    if (Object.keys(getBookmarkDto).length) {
+      return this.bookmarksService.find(getBookmarkDto);
+    }
     return this.bookmarksService.findAll();
+  }
+
+  @Get('/:id')
+  findById(@Param('id') id: string): Bookmark {
+    return this.bookmarksService.findById(id);
   }
 
   @Post()
--- src/bookmarks/bookmarks.service.ts
+++ src/bookmarks/bookmarks.service.ts
@@ -2,6 +2,7 @@ import { Bookmark } from './bookmark.model';
 import { Injectable } from '@nestjs/common';
 import { v4 as uuid } from 'uuid';
 import { CreatBookmarkDto } from './dto/create-bookmark.dto';
+import { GetBookmarkDto } from './dto/get-bookmark.dto';
 
 @Injectable()
 export class BookmarksService {
@@ -9,6 +10,26 @@ export class BookmarksService {
 
   findAll(): Bookmark[] {
     return this.bookmarks;
+  }
+
+  find(getBookmarkDto: GetBookmarkDto): Bookmark[] {
+    let bookmarks = this.findAll();
+    const { url, description } = getBookmarkDto;
+    if (url) {
+      bookmarks = bookmarks.filter((bookmarks) =>
+        bookmarks.url.toLowerCase().includes(url),
+      );
+    }
+    if (description) {
+      bookmarks = bookmarks.filter((bookmarks) =>
+        bookmarks.description.toLowerCase().includes(description),
+      );
+    }
+    return bookmarks;
+  }
+
+  findById(id: string): Bookmark {
+    return this.bookmarks.find((bookmark) => bookmark.id == id);
   }
 
   createBookmark(creatBookmarkDto: CreatBookmarkDto): Bookmark {
